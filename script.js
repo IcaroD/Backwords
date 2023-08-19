fcd7a0dc9a17b1f0a11edae65fdf831ba2e53714
IcaroD <icaro.davila.santos@gmail.com>
2023-08-19 17:32:01 -0300

    Backword

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function back(){
     var word = document.getElementById("area").value;
     var list = word.split('');
-
     list = list.reverse();
 
     document.getElementById("demo").innerHTML = list.join('');
